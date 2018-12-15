51f470794c322ec42b0cfafeec22468ef1700a50
Erika Ahlberg <erika.ahlberg@medieinstitutet.se>
2018-12-15 21:32:54 +0100

    fixed hero image

--- src/components/partials/HeroImage.vue
+++ src/components/partials/HeroImage.vue
@@ -1,13 +1,12 @@
 <template>
-<section id="hero">
-  <div class="hero-body flex-center" id="top-hero-img">
-      <div class="overlay">
+<section id="hero-img" class="hero is-link is-fullheight-with-navbar">
+  <div class="hero-body" id="top-hero-img">
+    <div class="overlay"></div>
     <div class="container">
       <h1>Karl Green</h1>
-      <h3 class="subtitle">
+      <h3 class="hej">
         Människa & vägledare på jorden
       </h3>
-    </div>
     </div>
   </div>
 </section>
@@ -27,15 +26,15 @@
     background: url("../../assets/images/karl.jpg"); 
     background-size: cover;
     position: relative;
-		height: 100%;
-    min-height: 350px;
-    max-height: 500px;
+		/*height: 100%;
+    min-height: 400px;
+    max-height: 500px;*/
     text-align: left;
-    .container{
+    /*.container{
       margin-top: 200px;
       display: inline-block;
       padding-left: 50px;
-    }
+    }*/
     .overlay{
         position: absolute;
         background: rgba(0, 0, 0, 0.543);
@@ -44,15 +43,19 @@
         top: 0;
         left: 0;
     } 
+    .container{
+      margin-bottom: -400px;
+      margin-left: 30px;
+    }
     .subtitle{
       font-weight: 400;
       color: white;
     }
-    i.fa-angle-right{
+    /*i.fa-angle-right{
       color: $red;
       font-size: 1.1em;
       position: relative;
       bottom: -4px;
-    }
+    }*/
   }
 </style>
