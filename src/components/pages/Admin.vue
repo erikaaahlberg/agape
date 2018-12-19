fcccec4f7e0dcf7b753a0a9ddb8da7b9c8f15400
Erika Ahlberg <erika.ahlberg@medieinstitutet.se>
2018-12-19 12:23:01 +0100

    installed bcryptjs

---
+++ src/components/pages/Admin.vue
@@ -0,0 +1,7 @@
+<template>
+</template>
+<script>
+import fetchBookings from '@/functions/fetchBookings.js';
+var bcrypt = require('bcryptjs');
+</script>
+<style lang="scss"></style>
--- src/functions/fetchBookings.js
+++ src/functions/fetchBookings.js
@@ -1,15 +1,23 @@
-export const fetchBookings = () => {
-    return fetch("http://localhost:3001/bookings", {
-       method: 'GET',
-       headers: {
-         'Accept': 'application/json',
-         'Content-Type': 'application/json',
-       }
-    })
-      .then(response => response.json())
-        .then((fetchedBookings) => {
-          console.log(fetchedBookings);
-          return fetchedBookings;
-          //this.bookings = fetchedBookings;
-        })
-  }
+export function fetchBookings () { 
+  /*axios.get('http://localhost:3001/bookings').then((res) => {
+      console.log('score?');
+      console.log(res);
+  });*/
+  fetch("http://localhost:3001/bookings", {
+    host: 'localhost',
+    // port to forward to
+    port:   3001,
+    // path to forward to
+    //path:   '/api/bookings',
+     method: 'GET',
+     headers: {
+       'Accept': 'application/json',
+       'Content-Type': 'application/json'
+     }
+  })
+    .then(response => response.json())
+      .then((fetchedBookings) => {
+        console.log(fetchedBookings);
+        this.bookings = fetchedBookings;
+      }) 
+}
